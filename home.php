1311b4f4d33e1e64db0761308147315b931a8f91
edaykin <fudaykin@gmail.com>
2015-12-07 00:18:47 +0000

    initial commit

---
+++ home.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/style.cs"/>
+</head>
+<body>
+ <div class="main_div">
+<h1> Hello </h1>
+<p> This is a github project testing page</p>
+</div>   
+</body>
+</html>
